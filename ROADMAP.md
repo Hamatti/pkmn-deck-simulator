403f26cb6ce93603026bf83467a1fee552da0cf1
Juha-Matti Santala <juhamattisantala@gmail.com>
2019-06-01 22:55:54 +0300

    Small fix & roadmap

---
+++ ROADMAP.md
@@ -0,0 +1,10 @@
+# ROADMAP
+*random notes for future ideas*
+
+## June 1st
+
+- Error display for invalid decks
+- Legal deck needs to have at least one basic
+- Build a view for deck (separate by type)
+- Simulation for opening hands
+- Build pipeline to Netlify
--- src/utils/cardamount-reducer.ts
+++ src/utils/cardamount-reducer.ts
@@ -3,11 +3,15 @@ import { DeckType, DetailType } from "../types";
 /**
  * Takes in a list of individual cards and reduces them into DeckType that
  * keeps track of the amount of each individual card
+
+ * [{id: 1, ...}, {id: 1, ...}, { id: 2, ...}]
+ * =>
+ * [{amount: 2, details: {id: 1, ...}}, { amount: 1, details: { id: 2, ...}}]
  */
 export const reduceWithAmounts = (cards: Array<DetailType>): DeckType => {
   return Object.values(
     cards.reduce((acc: DeckType, cur: DetailType) => {
-      if (Object.keys(acc).indexOf(cur.id.toString()) >= 0) {
+      if (existsInObject(cur.id.toString(), acc)) {
         acc[cur.id] = { ...acc[cur.id], amount: acc[cur.id].amount + 1 };
       } else {
         acc[cur.id] = { amount: 1, details: cur };
@@ -17,3 +21,6 @@ export const reduceWithAmounts = (cards: Array<DetailType>): DeckType => {
     }, {})
   );
 };
+
+const existsInObject = (needle, haystack): boolean =>
+  Object.keys(haystack).indexOf(needle) >= 0;
